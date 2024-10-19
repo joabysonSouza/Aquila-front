5d6a5cacb899eff5e423d6d0b5d4d0c1cc63661b
Joabyson <joabyson916@gmail.com>
2024-10-18 21:48:26 -0300

    refactor(register):adding route versioning

---
+++ app/v1/register/page.tsx
@@ -0,0 +1,28 @@
+import { NextPage } from "next";
+import React, { FormEvent } from "react";
+import Input from "../../Components/Input";
+import Button from "../../Components/Button";
+import VideoContainer from "../../Components/VideoBackground";
+
+const Register: NextPage<FormEvent> = () => {
+  return (
+    <VideoContainer>
+      <div className=" register mt-10  sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action="#" method="POST">
+          <Input label="Name" type="text" />
+          <Input label="Last Name" type="text" />
+          <Input label="Email" type="email" />
+          <Input label="Password" type="password" />
+          <Input label="Confirm password" type="password" />
+        </form>
+        <div className="mt-8">
+          <Button name="Register" type="submit" />
+        </div>
+      </div>
+    </VideoContainer>
+  );
+};
+
+
+export default Register;
+
