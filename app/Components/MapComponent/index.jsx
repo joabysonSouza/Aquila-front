3cae310c3c31e3ca386c91aad68173e1f251859b
Joabyson <joabyson916@gmail.com>
2024-10-22 22:09:30 -0300

    feat(MapComponent): "alterando para receber uma matriz de coordenadas"

--- app/Components/MapComponent/index.jsx
+++ app/Components/MapComponent/index.jsx
@@ -1,31 +1,42 @@
 // components/MapComponent.jsx
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import Modal from "../Modal";
 
 // Configurar o ícone do marcador
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: '/marker-icon-2x.png',
-    iconUrl: '/marker.svg',
-    shadowUrl: '/marker-shadow.png',
+  iconUrl: "/marker.svg",
+  shadowUrl: "/marker-shadow.svg",
 });
 
 const MapComponent = ({ coordinates }) => {
-    return (
-        <MapContainer center={coordinates} zoom={13} style={{ height: '400px', width: '100%' }}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
-            <Marker position={coordinates}>
-                <Popup>
-                    Coordenadas: {coordinates}
-                </Popup>
-            </Marker>
-        </MapContainer>
-    );
+
+  const center = coordinates.length > 0 ? coordinates[0] : [51.505, -0.09];
+
+  return (
+    <MapContainer
+      center={center}
+      zoom={3}
+      style={{ height: "800px", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {coordinates.map((coords, i) => (
+        <Marker position={coords} key={i}>
+          <Popup>
+            <Modal />
+            coordenadas : {coords[0]}, {coords[1]}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
 };
 
 export default MapComponent;
+
