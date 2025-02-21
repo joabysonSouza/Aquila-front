ced2bc0bcfbcd4a626e8a82ed5b1d8584527385a
Joabyson <joabyson916@gmail.com>
2025-02-20 23:20:05 -0300

    feat : adicionado o placeholder do input e o onChange para a captura da escrita

--- app/Components/Input/index.tsx
+++ app/Components/Input/index.tsx
@@ -1,13 +1,14 @@
 import React, { ChangeEvent } from "react";
 
 interface inputProps {
-  label: string;
+  label?: string;
   type: string;
   value: string | number;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string ;
 }
 
-export default function Input({ label, type, value }: inputProps) {
+export default function Input({ label, type, value,placeholder, onChange}: inputProps) {
   return (
     <div className="mx-3">
       <label
@@ -21,6 +22,8 @@ export default function Input({ label, type, value }: inputProps) {
           type={type}
           required
           value={value}
+          placeholder={placeholder}
+          onChange={onChange}
           className="block w-full rounded-md border-0 bg-slate-300 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
          step="any" />
       </div>
