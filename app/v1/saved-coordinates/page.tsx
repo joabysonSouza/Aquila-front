bc9ba9ee1d61765f9811f66efc841abcc0d1fac0
Joabyson <joabyson916@gmail.com>
2025-06-11 21:31:08 -0300

    Feat: "Criando a pagina que mostra as coordenadas salvas "

---
+++ app/v1/saved-coordinates/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import MapComponent from "@/app/Components/MapComponent";
+import React, { useEffect, useState } from "react";
+
+
+
+const savedCoordinates = () => {
+  const [coordinates, setCoordinates] = useState([]);
+
+
+
+  useEffect(() => {
+    const fetchCoordinates = async () => {
+      try {
+        const response = await fetch("http://localhost:3005/sensors");
+        const data = await response.json();
+
+        const coords = data.map((sensor:any) => {
+          const [lng, lat] = sensor.location.coordinates
+          return [lat,lng]
+        });
+        setCoordinates(coords);
+        console.log("aqui",coords,)
+
+      } catch (error) {
+        console.error("Erro ao buscar sensores:", error);
+      }
+    };
+    fetchCoordinates();
+  }, []);
+
+  return(
+    <MapComponent coordinates={coordinates} />
+
+  ) 
+};
+
+export default savedCoordinates;
