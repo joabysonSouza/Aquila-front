35041bceac36086ad81252492e5e7efc6366718b
Joabyson <joabyson916@gmail.com>
2025-07-14 22:48:40 -0300

    Feat: "criando o componente nameSensor aonde ele irá mostra o nome do sensor salvo"

--- app/Components/NameSensor/index.tsx
+++ app/Components/NameSensor/index.tsx
@@ -1,16 +1,40 @@
- import React from 'react'
- 
-  const NameSensor = ()=>{
+import React, { useEffect, useState } from 'react';
 
-    return (
-          <div className='w-80 text-red-700'>
-            <p>Esse é o meu sensor</p>
-       
-       </div>
-    )
-        
-    
+const NameSensor = ({ markerCoords}: {markerCoords:any}) => {
+  const [sensorName, setSensorName] = useState('');
 
- }
+  useEffect(() => {
+    const fetchCoordinates = async () => {
+      try {
+        const response = await fetch('http://localhost:3005/sensors');
+        const data = await response.json();
 
- export default NameSensor
+        const foundSensor = data.find((sensor: any) => {
+          const [lng, lat] = sensor.location.coordinates;
+          return (
+            lng === markerCoords[1] && lat === markerCoords[0]
+          );
+        });
+
+        if (foundSensor) {
+          setSensorName(foundSensor.sensor_name);
+        } else {
+          setSensorName('Nome não encontrado');
+        }
+      } catch (error) {
+        console.error('Erro ao buscar sensores:', error);
+        setSensorName('Erro ao buscar nome');
+      }
+    };
+
+    fetchCoordinates();
+  }, [markerCoords]);
+
+  return (
+    <div className='w-80 text-red-700'>
+      <p>Sensor: {sensorName}</p>
+    </div>
+  );
+};
+
+export default NameSensor;
