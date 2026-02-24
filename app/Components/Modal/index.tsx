947f17bb72e962a0fdce46a2f84abe3b151d388d
Joabyson <joabyson916@gmail.com>
2026-02-23 22:50:10 -0300

    Feat: "fazendo o fetch para uma api teste "

--- app/Components/Modal/index.tsx
+++ app/Components/Modal/index.tsx
@@ -43,7 +43,7 @@ const Modal = ({ showCoordinates, children }: ModalTypes) => {
     }
 
     try {
-      const response = await fetch("https://new-aquila-back.onrender.com/sensors", {
+      const response = await fetch("http://localhost:3005/sensors", {
         method: "POST",
         headers: {
           "content-Type": "application/json",
--- app/Components/NameSensor/index.tsx
+++ app/Components/NameSensor/index.tsx
@@ -6,7 +6,7 @@ const NameSensor = ({ markerCoords}: {markerCoords:any}) => {
   useEffect(() => {
     const fetchCoordinates = async () => {
       try {
-        const response = await fetch('https://new-aquila-back.onrender.com/sensors');
+        const response = await fetch('http://localhost:3005/sensors');
         const data = await response.json();
 
         const foundSensor = data.find((sensor: any) => {
